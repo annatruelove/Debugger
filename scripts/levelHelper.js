c23c9687e53fb46c8a3b7cf99cccfdfbabf5ee3d
Nikhil Komirisetti <nikhilk@live.unc.edu>
2020-02-02 13:31:45 -0500

    created level helper

---
+++ scripts/levelHelper.js
@@ -0,0 +1,22 @@
+const getScript = function (level) {
+    switch (level) {
+        case 1:
+            return level1Code;
+        case 2:
+            return level2Code;
+        case 3:
+            return level3Code;
+    }
+}
+
+const levelSpeed = function(level){
+    switch (level) {
+        case 1:
+            return 1000;
+        case 2:
+            return 750;
+        case 3:
+            return 500;
+    }
+
+}
